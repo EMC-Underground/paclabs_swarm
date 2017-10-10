495309b230cb0ec19c571f662d5e478be93ff03a
Craig J Smith <nctiggy@gmail.com>
2017-10-10 10:16:35 -0700

    more changes

--- files/install-docker.sh
+++ files/install-docker.sh
@@ -8,7 +8,7 @@ apt install -y \
     curl \
     software-properties-common \
     ntp \
-    libaio1 \
+    libaio1
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository \
@@ -22,7 +22,8 @@ systemctl daemon-reload
 systemctl restart docker
 usermod -a -G docker $USER
 curl -sSL https://dl.bintray.com/emccode/rexray/install | sh
-tar -xvf /tmp/EMC-ScaleIO-sdc-2.0-10000.2072.Ubuntu.16.04.x86_64.tar /tmp
+tar -xvf /tmp/EMC-ScaleIO-sdc-2.0-10000.2072.Ubuntu.16.04.x86_64.tar -C /tmp
+cd /tmp
 /tmp/siob_extract /tmp/EMC-ScaleIO-sdc-2.0-10000.2072.Ubuntu.16.04.x86_64.siob
 MDM_IP=192.168.0.120,192.168.0.129 dpkg -i /tmp/EMC-ScaleIO-sdc-2.0-10000.2072.Ubuntu.16.04.x86_64.deb
 mv /tmp/driver_sync.conf /bin/emc/scaleio/scini_sync/
