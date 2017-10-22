3dfd1b614295213c0b7db6c906fb5eb1b497c129
Craig J Smith <nctiggy@gmail.com>
2017-10-22 10:33:30 -0700

    updated sdc to 2.0-13000.211

--- files/setup.sh
+++ files/setup.sh
@@ -21,10 +21,10 @@ sed -i '/ExecStart/c\ExecStart=/usr/bin/dockerd -H 0.0.0.0:2375 -H fd://' /lib/s
 systemctl daemon-reload
 systemctl restart docker
 usermod -a -G docker $USER
-tar -xvf /tmp/EMC-ScaleIO-sdc-2.0-10000.2072.Ubuntu.16.04.x86_64.tar -C /tmp
+tar -xvf /tmp/EMC-ScaleIO-sdc-2.0-13000.211.Ubuntu.16.04.x86_64.tar -C /tmp
 cd /tmp
-/tmp/siob_extract /tmp/EMC-ScaleIO-sdc-2.0-10000.2072.Ubuntu.16.04.x86_64.siob
-dpkg -i /tmp/EMC-ScaleIO-sdc-2.0-10000.2072.Ubuntu.16.04.x86_64.deb
+/tmp/siob_extract /tmp/EMC-ScaleIO-sdc-2.0-13000.211.Ubuntu.16.04.x86_64.siob
+dpkg -i /tmp/EMC-ScaleIO-sdc-2.0-13000.211.Ubuntu.16.04.x86_64.deb
 mv /tmp/driver_sync.conf /bin/emc/scaleio/scini_sync/
-/bin/emc/scaleio/scini_sync/driver_sync.sh scini retrieve Ubuntu/2.0.10000.2072/`uname -r`
+/bin/emc/scaleio/scini_sync/driver_sync.sh scini retrieve Ubuntu/2.0.13000.211/`uname -r`
 service scini restart
